4c4ac09fcbc84018523269745f0bec0b2ac42b14
Chan Lee <Chanaomrithlee@gmail.com>
2023-10-13 12:48:10 -0700

    feat: modified external.js

--- js/external.js
+++ js/external.js
@@ -12,7 +12,8 @@ let brotherBear = prompt(`How many days would you like to rent Brother Bear?`);
 
 let dailyRate = prompt(`What is the daily rental rate?`);
 let totalCost = (littleMermaidDays + hercules + brotherBear) * dailyRate;
-alert(`You owe $` + totalCost);
+alert(`You owe $` + totalCost.toFixed(2));
+    //added toFixed to include 2 decimals-courtesy of classmate
 
 let googlePay = prompt(`How much is Google's pay per hour?`);
 let googleHrs = prompt(`How many hours did you work this week at Google?`);
@@ -21,4 +22,4 @@ let amazonHrs = prompt(`How many hour did you work this week at Amazon?`);
 let metaPay = prompt(`How much is Meta's pay per hour?`);
 let metaHrs = prompt(`How many hours did you work this week at Meta?`);
 let weekPay = ((googlePay * googleHrs) + (amazonPay * amazonHrs) + (metaPay * metaHrs));
-alert(`Your pay for this week is: $` + weekPay + `!`);
+alert(`Your pay for this week is: $` + weekPay.toFixed(2) + `!`);
