b4093b94d4c86ec7a24fd39a35149cfe6a1a3f38
Chan Lee <Chanaomrithlee@gmail.com>
2023-10-13 14:58:23 -0700

    feat: edited external.js

--- js/external.js
+++ js/external.js
@@ -5,21 +5,25 @@ alert(`Welcome to my Website!`);
 
 let userFavoriteColor = prompt(`What is your favorite color?`);
 alert(`Great, ` + userFavoriteColor + ` is my favorite color too!`);
+    /** can use ${userFavoriteColor} instead of concatenation;
+    *`Great, ${userFavoriteColor} is my favorite color too!`
+    */
 
-let littleMermaidDays = prompt(`How many days would you like to rent The Little Mermaid?`);
-let hercules = prompt(`How many days would you like to rent Hercules?`);
-let brotherBear = prompt(`How many days would you like to rent Brother Bear?`);
+let littleMermaidDays = parseFloat(prompt(`How many days would you like to rent The Little Mermaid?`));
+let hercules = parseFloat(prompt(`How many days would you like to rent Hercules?`));
+let brotherBear = parseFloat(prompt(`How many days would you like to rent Brother Bear?`));
+    //added parseFloat to change response to a number instead of a string
 
-let dailyRate = prompt(`What is the daily rental rate?`);
+let dailyRate = parseFloat(prompt(`What was the daily rental rate?`));
 let totalCost = (littleMermaidDays + hercules + brotherBear) * dailyRate;
-alert(`You owe $` + totalCost.toFixed(2));
-    //added toFixed to include 2 decimals-courtesy of classmate
+alert(`You owe $${totalCost.toFixed(2)}`);
+    //edited to use template literal
 
-let googlePay = prompt(`How much is Google's pay per hour?`);
-let googleHrs = prompt(`How many hours did you work this week at Google?`);
-let amazonPay = prompt(`How much is Amazon's pay per hour?`);
-let amazonHrs = prompt(`How many hour did you work this week at Amazon?`);
-let metaPay = prompt(`How much is Meta's pay per hour?`);
-let metaHrs = prompt(`How many hours did you work this week at Meta?`);
+let googlePay = parseFloat(prompt(`How much is Google's pay per hour?`));
+let googleHrs = parseFloat(prompt(`How many hours did you work this week at Google?`));
+let amazonPay = parseFloat(prompt(`How much is Amazon's pay per hour?`));
+let amazonHrs = parseFloat(prompt(`How many hour did you work this week at Amazon?`));
+let metaPay = parseFloat(prompt(`How much is Meta's pay per hour?`));
+let metaHrs = parseFloat(prompt(`How many hours did you work this week at Meta?`));
 let weekPay = ((googlePay * googleHrs) + (amazonPay * amazonHrs) + (metaPay * metaHrs));
-alert(`Your pay for this week is: $` + weekPay.toFixed(2) + `!`);
+alert(`Your pay for this week is: $${weekPay.toFixed(2)}!`);
