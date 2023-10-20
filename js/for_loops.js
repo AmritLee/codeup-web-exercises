81ed99fda14b2529d9004c76dbaf90a0b6371bf0
Chan Lee <Chanaomrithlee@gmail.com>
2023-10-20 09:10:20 -0700

    edit loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,26 +1,40 @@
-'use strict'
+"use strict";
 
-function showMultiplicationTable(inputNUM) {
-    for (let i = 1; i <= 10; i++) {
-        console.log(`${inputNUM} * ${i} = ${inputNUM * i}`);
+(() => {
+    function showMultiplicationTable(inputNUM) {
+        for (let i = 1; i <= 10; i++) {
+            console.log(`${inputNUM} * ${i} = ${inputNUM * i}`);
+        }
     }
-}
-showMultiplicationTable(7);
 
-for (let i = 1; i <= 10; i++) {
-    let randomNum = Math.floor(Math.random() * (200 - 20)) + 20;
-    if (randomNum % 2 === 0) {
-        console.log(`${randomNum} is even.`);
-    } else
-        console.log(`${randomNum} is odd.`);
-}
+    showMultiplicationTable(7);
 
-for (let i = 1; i <= 9; i++) {
-    let numStr = i.toString()
-    console.log(numStr.repeat(i));
-}
+    for (let i = 1; i <= 10; i++) {
+        let randomNum = Math.floor(Math.random() * (200 - 20 + 1)) + 20;
+        if (randomNum % 2 === 0) {
+            console.log(`${randomNum} is even.`);
+        } else
+            console.log(`${randomNum} is odd.`);
+    }
 
-for (let i = 100; i >= 5; i -= 5) {
-    console.log(i);
-}
+    for (let i = 1; i <= 9; i++) {
+        let numStr = i.toString()
+        console.log(numStr.repeat(i));
+    }
+    //for (let i = 1; i < 10; i++) {
+    //     let message = "";
+    //     for (let j = 0; j < 1; j++) {
+    //         message += i;
+    //         console.log(message);
+    //     }
+    //     }
+
+    for (let i = 100; i >= 5; i -= 5) {
+        console.log(i);
+    }
+})();
+//using IIFE
+
+
+
 
